284ad9121170668f8c3f983a6fc98272b47159db
XpAHiTeJl <robengud2@gmail.com>
2023-12-19 00:20:54 +0200

    CartComponent

--- src/components/Cart/Cart.ts
+++ src/components/Cart/Cart.ts
@@ -5,53 +5,85 @@ import { Product } from "../../schemas";
 
 export class Cart implements IComponent {
   private component: Component;
+  private description: Button;
+  private img: Component;
+  private count: Component;
+  private increase: Button;
+  private price: Component;
+  private deletebtn: Button;
 
   constructor(product: Product) {
-    const img = new Component({
+    this.img = new Component({
       tagName: "img",
       className: "image",
       attrs: {
         src: product.getImageURL(),
       },
-    }).getComponent();
+    });
 
-    const description = new Button({
+    this.description = new Button({
       tagName: "button",
       className: "description",
       textContent: " < ",
-    }).getComponent();
+    });
 
-    const count = new Component({
+    this.count = new Component({
       tagName: "p",
       className: "count",
       textContent: "0",
-    }).getComponent();
+    });
 
-    const increase = new Button({
+    this.increase = new Button({
       tagName: "button",
       className: "increase",
       textContent: " > ",
-    }).getComponent();
+    });
 
-    const price = new Component({
+    this.price = new Component({
       tagName: "p",
       className: "price",
       textContent: product.getPrice(),
-    }).getComponent();
+    });
 
-    const deletebtn = new Button({
+    this.deletebtn = new Button({
       tagName: "button",
       className: "deletebtn",
       textContent: "Delete",
-    }).getComponent();
+    });
 
     this.component = new Component({
       className: "cart",
-      children: [img, description, count, increase, price, deletebtn],
+      children: [
+        this.getComponent(),
+        this.getImg(),
+        this.getDescription(),
+        this.getCount(),
+        this.getIncrease(),
+        this.getPrice(),
+        this.getDeletebtn(),
+      ],
     });
   }
 
   getComponent(): HTMLElement {
     return this.component.getComponent();
+  }
+  getImg(): HTMLElement {
+    return this.img.getComponent();
+  }
+  getDescription(): HTMLElement {
+    return this.description.getComponent();
+  }
+  getCount(): HTMLElement {
+    return this.count.getComponent();
+  }
+  getIncrease(): HTMLElement {
+    return this.increase.getComponent();
+  }
+  getPrice(): HTMLElement {
+    return this.price.getComponent();
+  }
+  getDeletebtn(): HTMLElement {
+    return this.deletebtn.getComponent();
   }
 }
